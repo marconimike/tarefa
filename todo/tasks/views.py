ff31a1ab07344e20d5f25ca57c5a3b495abdcc4f
Marconi Pereira <marconiaula@gmail.com>
2022-09-29 14:21:19 -0300

    criação do templates, com páginas html. Integração de argumentos pela url

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,12 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def helloWorld(request):
+    return HttpResponse('Hello World!')
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html',{'name':name})
