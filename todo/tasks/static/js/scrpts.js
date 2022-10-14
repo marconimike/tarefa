c2bd6ca9e5c22e35e5c21a49d4c2b48fc99b548b
Marconi Pereira <marconiaula@gmail.com>
2022-10-13 21:44:46 -0300

    Filtrando as tarefas por status

--- todo/tasks/static/js/scrpts.js
+++ todo/tasks/static/js/scrpts.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
 
+    var baseUrl = 'http://127.0.0.1:8000/';
     var deleteBtn = $('.delete-btn');
     var searchBtn = $('#search-btn');
     var searchForm = $('#search-form');
+    var filter = $('#filter');
     
     $(deleteBtn).on('click', function(e) {
 
@@ -14,12 +16,19 @@ $( document ).ready(function() {
         if(result) {
             window.location.href = delLink;
         }
-
+    });
+    $(searchBtn).on('click', function() {
+    searchForm.subnit();
     });
 
+    $(filter).change(function() {
+        var filter = $(this).val();
+        window.location.href = baseUrl + '?filter=' + filter;
+    });
+
+    
+
 });
 
-$(searchBtn).on('click', function() {
-    searchForm.subnit();
-});
+
 
